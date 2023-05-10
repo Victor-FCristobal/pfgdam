99e46ea334bfee196070765ae04ec25ed185f453
Víctor Fernandez Cristobal <victorshouker@gmail.com>
2023-05-10 18:07:19 +0200

    Comienzo a entender acceso a datos

--- www/js/db.js
+++ www/js/db.js
@@ -1,2 +1,3 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-var db = window.sqlitePlugin.openDatabase({ name: 'gimnasiodb.db',location: 'default',androidDatabaseProvider: 'system'});
+document.addEventListener("deviceready", () => {
+    const db = window.sqlitePlugin.openDatabase({ name: 'gimnasiodb.db',location: 'default',androidDatabaseProvider: 'system'});
+}, false);
